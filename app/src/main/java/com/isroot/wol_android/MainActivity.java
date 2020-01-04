8778045fb601155ad603ace970f4b8f148e969d8
rootBear <dlwodnjs0727@gmail.com>
2020-01-05 02:06:41 +0900

    first commit

---
+++ app/src/main/java/com/isroot/wol_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.isroot.wol_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
