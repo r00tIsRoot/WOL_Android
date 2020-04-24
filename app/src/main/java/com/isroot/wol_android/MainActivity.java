18206c679c7399aa871bbeb1c11e0b04a2a08031
r00tIsRoot <dlwodnjs0727@gmail.com>
2020-04-24 13:23:27 +0900

    tmp commit

--- app/src/main/java/com/isroot/wol_android/MainActivity.java
+++ app/src/main/java/com/isroot/wol_android/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
+    GeofencingClient geofencingClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        geofencingClient = LocationServices.getGeofencingClient(this);
     }
 }
