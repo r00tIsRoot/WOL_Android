2fba2bcccc67b29753a07f640c6c4cb98ef22dbb
r00tIsRoot <dlwodnjs0727@gmail.com>
2020-05-03 10:58:36 +0900

    tmp commit

--- app/src/main/java/com/isroot/wol_android/MainActivity.java
+++ app/src/main/java/com/isroot/wol_android/MainActivity.java
@@ -1,21 +1,86 @@
 package com.isroot.wol_android;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.LocationServices;
+import com.isroot.wol_android.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     GeofencingClient geofencingClient;
+    ActivityMainBinding binding;
+    private static MainViewModel mViewModel;
+
+    public static MainViewModel getmViewModel() { return mViewModel; }
+    public void setmViewModel() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setViewModel(new MainViewModel(getApplication()));
+        mViewModel = binding.getViewModel();
+    }
+
+    private final String[] PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION};
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setmViewModel();
+
+        if (!isPermissionsGranted()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(this.PERMISSIONS, 0);
+            }
+        }
+
 
         geofencingClient = LocationServices.getGeofencingClient(this);
+    }
+
+
+    //여기부터는 GPS 활성화를 위한 메소드들
+    private void showDialogForLocationServiceSetting() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                + "위치 설정을 수정하실래요?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    private boolean isPermissionsGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (String permission : this.PERMISSIONS) {
+                if (checkSelfPermission(permission) == PackageManager.PERMISSION_DENIED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/isroot/wol_android/MainViewModel.java
@@ -0,0 +1,12 @@
+package com.isroot.wol_android;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainViewModel extends AndroidViewModel {
+        public MainViewModel(@NonNull Application application){
+            super(application);
+        }
+}
